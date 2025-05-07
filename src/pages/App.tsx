d09ac4435d134c8c78dd35f7d9aa21e31dd65045
lojaaaaa <lojaaaaa1@gmail.com>
2025-05-07 16:26:21 +0300

    feat: change app.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,7 @@ import { SecondComponent } from "@/features/SecongComponent";
 const App = () => {
     return (
         <>
-            <h1>Vite + React</h1>
+            <h1>Vite + React + 2</h1>
             <FirstComponent />
             <SecondComponent />
         </>
